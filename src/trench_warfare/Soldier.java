77f2efe54bb4b97cac89dbac254de9dddbd920a3
BBC132 <BBC132@BradyCampbells-MacBook-Air.local>
2015-11-13 15:00:12 -0800

    start Animator

--- src/trench_warfare/Soldier.java
+++ src/trench_warfare/Soldier.java
@@ -5,10 +5,13 @@
  */
 package trench_warfare;
 
+import images.Animator;
+import images.ImageManager;
 import images.ResourceTools;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
 
 /**
  *
@@ -20,6 +23,10 @@ public class Soldier {
     private SoldierState state = SoldierState.STAND_RIGHT;
     private SoldierType type;
     private Image image;
+    private Animator animator;
+//    private ImageManager imageManager;
+//            ArrayList<String> imageNames, int displayDurationMillis) {
+
 
     public Soldier(int x, int y, SoldierType type) {
         this.x = x;
@@ -40,7 +47,21 @@ public class Soldier {
         graphics.drawImage(getImage(), x, y, null);
     }
 
+    private static String STAND_LEFT = "STAND_LEFT";
+    private static String STAND_RIGHT = "STAND_RIGHT";
+    private static String RUN_01 = "RUN_01";
+    private static String RUN_02 = "RUN_02";
+    
     private void loadImages() {
+        String[] imageNames = {STAND_LEFT, STAND_RIGHT, RUN_01, RUN_02};
+        Image[] images;// = Image[4];
+        
+//        ImageManager imageManager = new ImageManager(imageNames, images);
+
+
+
+//animator
+        
         
         if (type == SoldierType.GREEN){
             this.image = ResourceTools.loadImageFromResource("trench_warfare/soldier_green_stand_left.gif");
